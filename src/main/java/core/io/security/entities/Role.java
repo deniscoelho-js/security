0662d06422d03f1c1304ddc6f57b766d5b343075
Denis Coelho <deniscoelho.dev@gmail.com>
2026-05-12 13:36:13 -0300

    att

--- src/main/java/core/io/security/entities/Role.java
+++ src/main/java/core/io/security/entities/Role.java
@@ -1,28 +1,33 @@
 package core.io.security.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Entity
 @Table(name = "tb_roles")
 @Setter
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "role_id")
-    private Long roleId;
-
+    private Long id;
     private String name;
 
+    public enum Values {
+        ADMIN(1L),
+        BASIC(2L);
 
-    
+        long roleId;
+
+        Values(long roleId) {
+            this.roleId = roleId;
+        }
+
+        public long getRoleId() {
+            return roleId;
+        }
+    }
 }
